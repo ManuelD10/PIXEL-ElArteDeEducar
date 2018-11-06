4c06a906ada48f69277de52a2589cc9878391fbd
Manuel Alejandro Daza <manuel.daza90@gmail.com>
2018-11-05 23:03:06 -0500

    Cambio de imágenes en los botones de nivel educativo
    Se agrega un archivo JS para que cambien las imágenes de Nivel Educativo al hacer hover en los botones del menú

---
+++ resources/js/nivelEducativo.js
@@ -0,0 +1,12 @@
+
+ function loadImagePrimary () {
+  document.getElementById('imgLevelEducation').src = "resources/images/boy-primary.png";
+ }
+
+ function loadImageBasic () {
+  document.getElementById('imgLevelEducation').src = "resources/images/boy-secondary.png";
+ }
+
+ function loadImageMedia () {
+  document.getElementById('imgLevelEducation').src = "resources/images/boy-media.png";
+ }
