cdd51e3c36c6eb78eec5cf96cfe71e32afaccda9
Manuel Alejandro Daza <manuel.daza90@gmail.com>
2018-11-06 01:07:17 -0500

    Se agregan los menús de los cursos y las materias

--- resources/js/nivelEducativo.js
+++ resources/js/nivelEducativo.js
@@ -10,3 +10,79 @@
  function loadImageMedia () {
   document.getElementById('imgLevelEducation').src = "resources/images/boy-media.png";
  }
+
+
+//--- basicaSecundaria -------
+  function numeroPrimero() {
+    document.getElementById('txtPrimero').style = "width:100%";
+  }
+  function cierreNumeroPrimero() {
+    document.getElementById('txtPrimero').style = "width:0%";
+  }
+
+  function numeroSegundo() {
+    document.getElementById('txtSegundo').style = "width:100%";
+  }
+  function cierreNumeroSegundo() {
+    document.getElementById('txtSegundo').style = "width:0%";
+  }
+  function numeroTercero() {
+    document.getElementById('txtTercero').style = "width:100%";
+  }
+  function cierreNumeroTercero() {
+    document.getElementById('txtTercero').style = "width:0%";
+  }
+  function numeroCuarto() {
+    document.getElementById('txtCuarto').style = "width:100%";
+  }
+  function cierreNumeroCuarto() {
+    document.getElementById('txtCuarto').style = "width:0%";
+  }
+  function numeroQuinto() {
+    document.getElementById('txtQuinto').style = "width:100%";
+  }
+  function cierreNumeroQuinto() {
+    document.getElementById('txtQuinto').style = "width:0%";
+  }
+
+  function numeroSexto() {
+    document.getElementById('txtSexto').style = "width:100%";
+  }
+  function cierreNumeroSexto() {
+    document.getElementById('txtSexto').style = "width:0%";
+  }
+
+  function numeroSeptimo() {
+    document.getElementById('txtSeptimo').style = "width:100%";
+  }
+  function cierreNumeroSeptimo() {
+    document.getElementById('txtSeptimo').style = "width:0%";
+  }
+
+  function numeroOctavo() {
+    document.getElementById('txtOctavo').style = "width:100%";
+  }
+  function cierreNumeroOctavo() {
+    document.getElementById('txtOctavo').style = "width:0%";
+  }
+
+  function numeroNoveno() {
+    document.getElementById('txtNoveno').style = "width:100%";
+  }
+  function cierreNumeroNoveno() {
+    document.getElementById('txtNoveno').style = "width:0%";
+  }
+
+  function numeroDecimo() {
+    document.getElementById('txtDecimo').style = "width:100%";
+  }
+  function cierreNumeroDecimo() {
+    document.getElementById('txtDecimo').style = "width:0%";
+  }
+
+  function numeroOnce() {
+    document.getElementById('txtOnce').style = "width:100%";
+  }
+  function cierreNumeroOnce() {
+    document.getElementById('txtOnce').style = "width:0%";
+  }
